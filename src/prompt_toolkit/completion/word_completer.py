060f319107b3ae18cde76f7c943b829621b5e42c
t1babin <t1babin@student.bridgew.edu>
2023-04-05 18:24:24 -0400

    sorted imports alphabetically

--- src/prompt_toolkit/completion/word_completer.py
+++ src/prompt_toolkit/completion/word_completer.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
+from keyboard import press_and_release
 from typing import Callable, Iterable, Mapping, Pattern
 
 from prompt_toolkit.completion import CompleteEvent, Completer, Completion
 from prompt_toolkit.document import Document
 from prompt_toolkit.formatted_text import AnyFormattedText
-from keyboard import press_and_release
 
 __all__ = [
     "WordCompleter",
