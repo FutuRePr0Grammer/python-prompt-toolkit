39fc55892b37f4f7c9e65671f1c33ecf1935a49c
PenguinZay <i1walker@student.bridgew.edu>
2023-05-05 13:07:25 -0400

    One more commit with the alternative to fix 909.

--- examples/test1609.py
+++ examples/test1609.py
@@ -1,7 +1,7 @@
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter
 
-mycompleter = WordCompleter(['-a', '-app', '--apple', '--apricot' ,'build', 'builder', 'buildx'])
+mycompleter = WordCompleter(['-a', '-app', '--apple', '--apricot' ,'build', 'builder', 'buildx'], move_back_one_space=False)
 
 text = prompt('prompt: ', completer=mycompleter)
 print('You said: %s' % text)
