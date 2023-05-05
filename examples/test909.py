c2828650e9df809f58cd51aebe9ca0b4939eee45
David Rochon III <59246317+FutuRePr0Grammer@users.noreply.github.com>
2023-05-05 10:50:30 -0400

    Refactored the code in test909 and added more test cases. Also added test test_word_completer_static_word_list_long_words to test_completion.py. However, a manual test plan is still needed simply due to the fact that the ellipsis is only added in the command line, and that cannot be tested in a unit test.

--- examples/test909.py
+++ examples/test909.py
@@ -1,7 +1,8 @@
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter
 
-mycompleter = WordCompleter(['reeeeeeeeeallyreallyreallyreallylong.word.test.completion_test_trying_to_add_ellipsis123456763322323243asdfasdsdf323232'])
+mycompleter = WordCompleter(['reeeeeeeeeallyreallyreallyreallylong.word.test.completion_test_trying_to_add_ellipsis123456763322323243asdfasdsdf323232',
+                             'reallytestshortstring', 'testunrelatedstring'])
 
 text = prompt('prompt: ', completer=mycompleter)
 print('You said: %s' % text)
