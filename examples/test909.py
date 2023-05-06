9cf0440e04ee1f6ce9a024a97c843583881ad3fe
PenguinZay <i1walker@student.bridgew.edu>
2023-05-06 16:22:17 -0400

    Issue 909 Complete: Just added a couple more strings to test for in the in-class presentation on May 9th.

--- examples/test909.py
+++ examples/test909.py
@@ -2,7 +2,9 @@ from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter
 
 mycompleter = WordCompleter(['reeeeeeeeeallyreallyreallyreallylong.word.test.completion_test_trying_to_add_ellipsis123456763322323243asdfasdsdf323232',
-                             'reallytestshortstring', 'testunrelatedstring', 'AUTO_COMPLETE_A_VERY_LONG_STRING_BECAUSE_SOMETIMES_THIS_HAPPENS'])
+                             'AUTO_COMPLETE_A_VERY_LONG_STRING_BECAUSE_SOMETIMES_THIS_HAPPENSAUTO_COMPLETE_A_VERY_LONG_STRING_BECAUSE_SOMETIMES_THIS_HAPPENSAUTO_COMPLETE_A_VERY_LONG_STRING_BECAUSE_SOMETIMES_THIS_HAPPENS2'
+                             'reallytestshortstring', 'testunrelatedstring',
+                             'welcome to the test!', 'wow this is fun', 'wonderful ways to win'])
 
 text = prompt('prompt: ', completer=mycompleter)
 print('You said: %s' % text)
